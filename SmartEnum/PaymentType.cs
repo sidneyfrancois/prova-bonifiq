e2b66cbe47f8fae3a5a1db6dbbb21a944899f167
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-13 13:40:22 -0300

    add class for different types of payments

--- SmartEnum/PaymentType.cs
+++ SmartEnum/PaymentType.cs
@@ -4,5 +4,24 @@
     {
         private PaymentType(int value, string displayName) : base(value, displayName) { }
         private PaymentType() { }
+
+        public static readonly PaymentType Pix = new PixPayment();
+        public static readonly PaymentType CreditCard = new CreditCardPayment();
+        public static readonly PaymentType Paypal = new PaypalPayment();
+
+        private sealed class PixPayment : PaymentType
+        {
+            public PixPayment() : base(0, "Pix") { }
+        }
+
+        private sealed class CreditCardPayment : PaymentType
+        {
+            public CreditCardPayment() : base(1, "Cartão de crédito") { }
+        }
+
+        private sealed class PaypalPayment: PaymentType
+        {
+            public PaypalPayment() : base(2, "Paypal") { }
+        }
     }
 }
