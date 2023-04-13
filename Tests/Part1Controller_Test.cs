a663ed56fb05e6a2c536b8c0f9ffff3fb3eea3dc
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-13 09:48:24 -0300

    add test for the first challange (Part1Controller)

---
+++ Tests/Part1Controller_Test.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace ProvaPub.Tests
+{
+    public class Part1Controller_Test
+    {
+        [Fact]
+        public void RandomService_GetRandom_CheckRandomnessReturn()
+        {
+
+        }
+    }
+}
