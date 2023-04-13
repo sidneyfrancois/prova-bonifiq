2eae50bc6482c2debe19e49ad726e7bd61f68b28
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-13 11:36:04 -0300

    create PaymentType based based on SmartEnumeration approach

---
+++ SmartEnum/PaymentType.cs
@@ -0,0 +1,12 @@
+﻿namespace ProvaPub.SmartEnum
+{
+    public class PaymentType : Enumeration
+    {
+        private PaymentType(int value, string displayName) : base(value, displayName) { }
+        private PaymentType() { }
+
+        public static readonly PaymentType Pix = new PaymentType(0, "Pix");
+        public static readonly PaymentType CreditCard = new PaymentType(1, "Cartão de crédito");
+        public static readonly PaymentType Paypal = new PaymentType(2, "Paypal");
+    }
+}
