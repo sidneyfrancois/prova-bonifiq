2d436aee5cf71f181d59de45fa892b40c20c9f65
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-13 15:09:15 -0300

    payment based on paymentMethod variable

--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -5,12 +5,13 @@ namespace ProvaPub.Services
 {
 	public class OrderService
 	{
-		public async Task<Order> PayOrder(PaymentTypeEnum paymentMethod, decimal paymentValue, int customerId)
+		public async Task<Order> PayOrder(string paymentMethod, decimal paymentValue, int customerId)
 		{
-			var paymentT = PaymentType.FromValue((int)paymentMethod);
+			// get paymentMethod based on the string value
+			var paymentT = PaymentType.FromDisplayName(paymentMethod);
+
+			// do the payment using the method of the specified payment method
 			var result = paymentT.FazerPagamento(paymentValue);
-
-
 
 			return await Task.FromResult( new Order()
 			{
