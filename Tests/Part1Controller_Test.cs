ca907c76919165eeb0bee0981e40cff0bb80869d
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-13 10:19:25 -0300

    add simple to test randomness using factory of RandomService, not perfect, must improve randomness

--- Tests/Part1Controller_Test.cs
+++ Tests/Part1Controller_Test.cs
@@ -1,13 +1,24 @@
-﻿using Xunit;
+﻿using ProvaPub.Services;
+using Xunit;
 
 namespace ProvaPub.Tests
 {
     public class Part1Controller_Test
     {
+        private RandomService FactoryRandomService() => new RandomService();
+
         [Fact]
         public void RandomService_GetRandom_CheckRandomnessReturn()
         {
+            // Arrange
 
+            // Act 
+            var randomNumbers = Enumerable.Range(0, 20)
+                .Select(i => FactoryRandomService().GetRandom())
+                .ToList();
+
+            // Assert
+            Assert.Equal(randomNumbers.Count, randomNumbers.Distinct().Count());
         }
     }
 }
