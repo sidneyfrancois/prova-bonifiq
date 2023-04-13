4b7221f1b921cf1ead0a199b544614e7c6cec1a8
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-13 10:57:29 -0300

    use PaymentType enum as parameter, multiple if statements not refactored yet.

--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -6,15 +6,15 @@ namespace ProvaPub.Services
 	{
 		public async Task<Order> PayOrder(PaymentType paymentMethod, decimal paymentValue, int customerId)
 		{
-			if (paymentMethod == "pix")
+			if (paymentMethod == PaymentType.Pix)
 			{
 				//Faz pagamento...
 			}
-			else if (paymentMethod == "creditcard")
+			else if (paymentMethod == PaymentType.CreditCard)
 			{
 				//Faz pagamento...
 			}
-			else if (paymentMethod == "paypal")
+			else if (paymentMethod == PaymentType.CreditCard)
 			{
 				//Faz pagamento...
 			}
