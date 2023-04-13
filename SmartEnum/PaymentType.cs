2b65d9a00dda20db366f25542935c4d589e136d5
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-13 13:32:38 -0300

    add type to Enumeration abstract class

--- SmartEnum/PaymentType.cs
+++ SmartEnum/PaymentType.cs
@@ -1,12 +1,8 @@
 ﻿namespace ProvaPub.SmartEnum
 {
-    public class PaymentType : Enumeration
+    public abstract class PaymentType : Enumeration<PaymentType>
     {
         private PaymentType(int value, string displayName) : base(value, displayName) { }
         private PaymentType() { }
-
-        public static readonly PaymentType Pix = new PaymentType(0, "Pix");
-        public static readonly PaymentType CreditCard = new PaymentType(1, "Cartão de crédito");
-        public static readonly PaymentType Paypal = new PaymentType(2, "Paypal");
     }
 }
