eb76812fe2aae7abb03071614c6a6943709f3dde
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-14 19:26:42 -0300

    use fixture and add assert to verify range using the id

--- Tests/Parte2Controller_Test.cs
+++ Tests/Parte2Controller_Test.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Moq;
+using NuGet.Frameworks;
 using ProvaPub.Controllers;
 using ProvaPub.Models;
 using ProvaPub.Repository;
@@ -9,8 +10,16 @@ using Xunit;
 
 namespace ProvaPub.Tests
 {
+    [Collection(nameof(ProductContext))]
     public class Parte2Controller_Test
     {
+        private readonly Parte2Controller_Fixture_Test _productFixture;
+
+        public Parte2Controller_Test(Parte2Controller_Fixture_Test productFixture)
+        {
+            _productFixture = productFixture;
+        }
+
         [Theory(DisplayName = "Teste de ListProducts")]
         [Trait("Parte2Controller", "Parte2 Mock Test")]
         [InlineData(1)]
@@ -18,21 +27,14 @@ namespace ProvaPub.Tests
         public void Parte2Controller_ListProducts_ReturnSucess(int page)
         {
             // Arrange
-            var options = new DbContextOptionsBuilder<TestDbContext>()
-                .UseInMemoryDatabase(databaseName: "ProductListDatabase")
-                .Options;
-
-            var contextDb = new TestDbContext(options);
-            var products = contextDb.getProductSeed();
-            contextDb.Products.AddRange(products);
-            contextDb.SaveChanges();
-            var productService = new ProductService(contextDb);
+            var productService = new ProductService(_productFixture.productsContext);
 
             // Act
             ProductList listResult = productService.ListProducts(page);
 
             // Assert
-            Assert.Equal(page*10, listResult.Products.Count);
+            Assert.Equal(10, listResult.Products.Count);
+            Assert.Equal(((page*10)), listResult.Products.Last().Id);
         }
     }
 }
