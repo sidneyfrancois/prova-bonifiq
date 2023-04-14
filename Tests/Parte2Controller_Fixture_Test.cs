6ccc3253a9c3a5871edca6fe5bea80d47f4bdeaf
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-14 19:25:43 -0300

    create test fixture to instanciate context with dataset

---
+++ Tests/Parte2Controller_Fixture_Test.cs
@@ -0,0 +1,41 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using ProvaPub.Models;
+using ProvaPub.Repository;
+using Xunit;
+
+
+namespace ProvaPub.Tests
+{
+    [CollectionDefinition(nameof(ProductContext))]
+    public class ProductContext: ICollectionFixture<Parte2Controller_Fixture_Test>
+    {}
+
+
+    public class Parte2Controller_Fixture_Test : IDisposable
+    {
+        public TestDbContext productsContext { get; set; }
+
+        public Parte2Controller_Fixture_Test()
+        {
+            productsContext = GenerateContext();
+        }
+
+        public TestDbContext GenerateContext()
+        {
+            var options = new DbContextOptionsBuilder<TestDbContext>()
+                .UseInMemoryDatabase(databaseName: "ProductListDatabase")
+                .Options;
+
+            var contextDb = new TestDbContext(options);
+            contextDb.Products.AddRange(contextDb.getProductSeed());
+            contextDb.SaveChanges();
+            return contextDb;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
