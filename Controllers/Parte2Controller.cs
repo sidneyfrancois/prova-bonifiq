b917a49ccad909f2c2e65020fb924bd05d024d40
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-14 18:13:24 -0300

    remove context dependency, add service

--- Controllers/Parte2Controller.cs
+++ Controllers/Parte2Controller.cs
@@ -18,24 +18,25 @@ namespace ProvaPub.Controllers
 		/// Como você faria pra criar uma estrutura melhor, com menos repetição de código? E quanto ao CustomerService/ProductService. Você acha que seria possível evitar a repetição de código?
 		/// 
 		/// </summary>
-		TestDbContext _ctx;
-		public Parte2Controller(TestDbContext ctx)
+		private readonly ProductService _productService;
+		private readonly CustomerService _customerService;
+		
+		public Parte2Controller(ProductService productService, CustomerService customerService)
 		{
-			_ctx = ctx;
+			_productService = productService;
+			_customerService = customerService;
 		}
 	
 		[HttpGet("products")]
 		public ProductList ListProducts(int page)
 		{
-			var productService = new ProductService(_ctx);
-			return productService.ListProducts(page);
+			return _productService.ListProducts(page);
 		}
 
 		[HttpGet("customers")]
 		public CustomerList ListCustomers(int page)
 		{
-			var customerService = new CustomerService(_ctx);
-			return customerService.ListCustomers(page);
+			return _customerService.ListCustomers(page);
 		}
 	}
 }
