b06c790d2a64146f15619f54a6f599b548d8f2a4
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-15 09:52:41 -0300

    refactor business rules into different functions.

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -15,11 +15,11 @@ namespace ProvaPub.Services
 
         public List<Customer> ListCustomers(int page)
         {
-			var customerPageList = new PageList<Customer>();
+            var customerPageList = new PageList<Customer>();
 
-			customerPageList.ResultPageList = _ctx.Customers.Skip((page - 1) * customerPageList.TotalCount).Take(customerPageList.TotalCount).ToList();
+            customerPageList.ResultPageList = _ctx.Customers.Skip((page - 1) * customerPageList.TotalCount).Take(customerPageList.TotalCount).ToList();
 
-			return customerPageList.ResultPageList;
+            return customerPageList.ResultPageList;
         }
 
         public async Task<bool> CanPurchase(int customerId, decimal purchaseValue)
@@ -29,21 +29,36 @@ namespace ProvaPub.Services
             if (purchaseValue <= 0) throw new ArgumentOutOfRangeException(nameof(purchaseValue));
 
             //Business Rule: Non registered Customers cannot purchase
-            var customer = await _ctx.Customers.FindAsync(customerId);
-            if (customer == null) throw new InvalidOperationException($"Customer Id {customerId} does not exists");
+            await CheckCustomerExistence(customerId);
 
             //Business Rule: A customer can purchase only a single time per month
+            await CheckPurchaseValidation(customerId);
+
+            //Business Rule: A customer that never bought before can make a first purchase of maximum 100,00
+            await CheckCustomerIsFirstPurchase(customerId, purchaseValue);
+
+            return true;
+        }
+
+        private async Task CheckCustomerIsFirstPurchase(int customerId, decimal purchaseValue)
+        {
+            var haveBoughtBefore = await _ctx.Customers.CountAsync(s => s.Id == customerId && s.Orders.Any());
+            if (haveBoughtBefore == 0 && purchaseValue > 100)
+                throw new InvalidOperationException($"Customer {customerId} can make a first purchase of maximum 100,00");
+        }
+
+        private async Task CheckPurchaseValidation(int customerId)
+        {
             var baseDate = DateTime.UtcNow.AddMonths(-1);
             var ordersInThisMonth = await _ctx.Orders.CountAsync(s => s.CustomerId == customerId && s.OrderDate >= baseDate);
             if (ordersInThisMonth > 0)
-                return false;
+                throw new InvalidOperationException($"Customer {customerId} can't make more purchases in this mounth");
+        }
 
-            //Business Rule: A customer that never bought before can make a first purchase of maximum 100,00
-            var haveBoughtBefore = await _ctx.Customers.CountAsync(s => s.Id == customerId && s.Orders.Any());
-            if (haveBoughtBefore == 0 && purchaseValue > 100)
-                return false;
-
-            return true;
+        private async Task CheckCustomerExistence(int customerId)
+        {
+            var customer = await _ctx.Customers.FindAsync(customerId);
+            if (customer == null) throw new InvalidOperationException($"Customer Id {customerId} does not exists");
         }
 
     }
