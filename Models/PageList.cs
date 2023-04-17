0b489ca82de2e448a36f158b7a6fcb4c80eab6a2
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-16 21:55:17 -0300

    refactor to a generic abstract class to return the page list

--- Models/PageList.cs
+++ Models/PageList.cs
@@ -1,9 +1,25 @@
-﻿namespace ProvaPub.Models
+﻿using Microsoft.EntityFrameworkCore;
+using ProvaPub.Repository;
+using System.Threading;
+
+namespace ProvaPub.Models
 {
-    public class PageList<T>
-    {
-		public List<T> ResultPageList { get; set; } = new List<T>();
+    public abstract class PageList<T> where T : class
+    { 
         public int TotalCount { get; set; } = 10;
-        public bool HasNext { get; set; } = true;
+
+        public List<T> ListPage(int page, TestDbContext ctx)
+        {
+            List<T> pageList = ctx.Set<T>()
+                                .Skip((page - 1) * TotalCount)
+                                .Take(TotalCount)
+                                .ToList();
+
+            if (pageList.Count == 0)
+                throw new InvalidOperationException($"There is no {nameof(T)} in specific page");
+
+            return pageList;
+        }
     }
 }
+
